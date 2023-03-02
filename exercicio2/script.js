371c7abd2dc71aa8fb2df858bfba7c0d36f75526
Amanda <amandaoliveirapolari@gmail.com>
2023-03-02 10:31:41 -0300

    Exercício 2 - ok!

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,23 @@
-let nacionalidade = prompt("Digite aqui a nacionalidade").toLowerCase()
+let nacionalidade = prompt('Digite aqui a nacionalidade').toLowerCase();
+const mensagemPositiva = `A sua nacionalidade é ${nacionalidade}`;
+const mensagemNegativa = `A nacionalidade '${nacionalidade}' não foi encontrada`;
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!")
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!")
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!")
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!")
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!")
-} else{
-    console.log("nacionalidade não encontrada")
+switch (nacionalidade) {
+    case 'brasileira':
+        console.log(mensagemPositiva);
+        break;
+    case 'argentina':
+        console.log(mensagemPositiva);
+        break;
+    case 'uruguaia':
+        console.log(mensagemPositiva);
+        break;
+    case 'chilena':
+        console.log(mensagemPositiva);
+        break;
+    case 'colombiana':
+        console.log(mensagemPositiva);
+        break;
+    default:
+        console.log(mensagemNegativa);
 }

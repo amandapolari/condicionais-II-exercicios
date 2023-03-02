08ed703cfe52ad7608b09c9348e1b497148fa11e
Amanda <amandaoliveirapolari@gmail.com>
2023-03-02 15:47:26 -0300

    Correção do exercício 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,21 +1,21 @@
 const num = Number(prompt('Digite um número'));
-let msgPositiva = `O número ${num} É divisível por 2 e por 3`;
-let msgNegativa = `O número ${num} NÃO é divisível por 2 e por 3`;
+let msgPositiva = `O número ${num} É divisível por 2 ou por 3`;
+let msgNegativa = `O número ${num} NÃO é divisível por 2 ou por 3`;
 
 // if com aninhamento:
-if (num % 2 === 0) {
-    if (num % 3 === 0) {
-        console.log(msgPositiva);
-    } else {
+if (num % 2 !== 0) {
+    if (num % 3 !== 0) {
         console.log(msgNegativa);
+    } else {
+        console.log(msgPositiva);
     }
 } else {
-    console.log(msgNegativa);
+    console.log(msgPositiva);
 }
 
 // if com operador lógico:
-// if (num % 2 === 0 && num % 3 === 0) {
-//     console.log(msgPositiva);
-// } else {
-//     console.log(msgNegativa);
-// }
+if (num % 2 === 0 || num % 3 === 0) {
+    console.log(msgPositiva);
+} else {
+    console.log(msgNegativa);
+}
